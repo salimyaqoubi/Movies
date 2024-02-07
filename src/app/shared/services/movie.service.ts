c12c372f4779c0c0be151e5a3f9a97a6927603e3
salim <salimyaaqoubii@gmail.com>
2024-02-07 15:33:45 +0400

    added

---
+++ src/app/shared/services/movie.service.ts
@@ -0,0 +1,63 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+const options = {
+  params: {
+    include_adult: 'false',
+    include_video: 'true',
+    language: 'en-US',
+    page: '1',
+    sort_by: 'popularity.desc'
+  },
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMDJhNDE1MzlhNzg1NTNiNmY4MjEyYWQ5ZjAzOGNiNCIsInN1YiI6IjY1YzM0ZWZjMTU3OTRhMDE2MjU1MDZlYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xNNOgU5MVPX0mwVRdFcVSOLygvB4hvuYR_yH8xVTQao'
+  }
+}
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieService {
+
+  http = inject(HttpClient);
+
+  getMovies() {
+    return this.http.get<any>('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc', options)
+  }
+
+  getTvShows() {
+    return this.http.get('https://api.themoviedb.org/3/discover/tv?include_adult=false&include_null_first_air_dates=false&language=en-US&page=1&sort_by=popularity.desc', options)
+  }
+
+  getRatedMovies() {
+    return this.http.get('https://api.themoviedb.org/3/guest_session/guest_session_id/rated/movies', options)
+  }
+
+  getBannerImage(id: number) {
+    return this.http.get(`https://api.themoviedb.org/3/movie/${id}/images`, options)
+  }
+
+  getBannerVideo(id: number) {
+    return this.http.get(`https://api.themoviedb.org/3/movie/${id}/videos`, options);
+  }
+
+  getBannerDetail(id: number) {
+    return this.http.get(`https://api.themoviedb.org/3/movie/${id}`, options);
+  }
+
+  getNowPlayingMovies() {
+    return this.http.get('https://api.themoviedb.org/3/movie/now_playing', options)
+  }
+
+  getPopularMovies() {
+    return this.http.get('https://api.themoviedb.org/3/movie/popular', options)
+  }
+
+  getTopRated() {
+    return this.http.get('https://api.themoviedb.org/3/movie/top_rated', options)
+  }
+
+  getUpcomingMovies() {
+    return this.http.get('https://api.themoviedb.org/3/movie/upcoming', options)
+  }
+}
